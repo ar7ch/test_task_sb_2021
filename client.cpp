af21408fcb52ae03418d5f763efe6ec749ef0082
Artem <a.bulgakov@edu.miet.ru>
2021-07-04 23:26:24 +0300

    implement test client app

---
+++ client.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdio>
+#include <filesystem>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
+
+
+using namespace std;
+
+class Client {
+	
+	public:
+	void init_client(){
+		this->inter_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+		if(this->inter_fd == -1) {
+			perror("socket");
+			exit(EXIT_FAILURE);
+		}
+		//memset(&(this->inter_addr), 0, sizeof(struct sockaddr_un));
+		this->inter_addr.sun_family = AF_UNIX;
+		strcpy(this->inter_addr.sun_path, this->socket_addr); // FIXME unsafe
+	}
+	void connect_server() {
+		socklen_t struct_len = strlen(this->inter_addr.sun_path) + sizeof(this->inter_addr.sun_family);
+		if (connect(this->inter_fd, (struct sockaddr *) &(this->inter_addr), struct_len) == -1){
+			perror("connect");
+			exit(EXIT_FAILURE);
+		}
+		cout << "Connected to server" << endl;
+	}
+	
+	void io_server() {
+		char message[20] = "Hello world\n";
+		if (send(this->inter_fd, message, strlen(message), 0) == -1) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}
+		close(this->inter_fd);
+	}
+
+	private:
+	int inter_fd;
+	struct sockaddr_un inter_addr;
+	char socket_addr[160]="/home/artem/task1/scan_service.socket";
+		
+};
+
+int main(int argc, char ** argv) {
+    if (argc != 2) { 
+		cerr << "usage: " << argv[0] << " <path to directory>" << endl;
+        return EXIT_FAILURE;
+    }
+    else {
+        try {
+			const string path(argv[1]);
+			if (!filesystem::exists(path)) throw invalid_argument("Path does not exist");	
+			Client client;
+			client.init_client();
+			client.connect_server();
+			client.io_server();
+        }
+        catch (const invalid_argument& ia) {
+            cerr << "ERROR: " << ia.what() << endl; 
+            return EXIT_FAILURE;
+        }
+    }
+    return 0;
+}
